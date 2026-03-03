8547a3dc88923d2895b757105acf66d9e63b1d0a
Suhas Cristy Mathey <132161396+Cristy210@users.noreply.github.com>
2026-03-03 08:55:26 -0500

    TSC Python Implementation -- MLX

---
+++ TSC_Python/tsc_mlx.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import Iterable, List, Optional, Sequence, Tuple, Union
+
+import numpy as np
+import mlx.core as mx
+from tqdm.auto import trange
+
+Array = mx.array
